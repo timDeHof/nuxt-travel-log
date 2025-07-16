61a7bd04eabe18c9bcde3a9cdca898f69cd3dde6
timDeHof <ttdehof@gmail.com>
2025-07-15 22:11:52 -0400

    feat(auth): add logout icon

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -24,6 +24,7 @@ const authStore = useAuthStore();
     <ul tabindex="0" class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
       <li>
         <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout" size="24" />
           Sign Out
         </NuxtLink>
       </li>
