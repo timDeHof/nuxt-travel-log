fe0bbd4edea9b81e3086eea864b2bf1d3a516514
Timothy Tyler DeHof <ttdehof@gmail.com>
2025-07-15 22:25:04 -0400

    feat(auth): :sparkles: add auth state/sign out (#56)

---
+++ pages/sign-out.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+onMounted(() => authStore.signOut());
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center">
+    <span class="loading loading-spinner loading-xl" />
+  </div>
+</template>
